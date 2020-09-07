1fa3ae6adbe6b31a75e224da7c2fec69c34c9e12
Gledys <gdys92@gmail.com>
2020-09-07 16:22:57 -0300

    prueba 1

--- app/src/main/java/com/example/poc/MainActivity.java
+++ app/src/main/java/com/example/poc/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.poc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        url="http://www.saij.gob.ar/";
+        //url="https://192.168.0.1";
     }
-}
+
+    public void abrir(View view){
+
+        Intent intent; //declaro el intent
+        intent= new Intent(this, Main2Activity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+
+
+    }
+
+
+
+
