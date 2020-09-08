01e842c1f0216a1117a2026d56872fb6cef9be88
Gledys <gdys92@gmail.com>
2020-09-08 11:50:56 -0300

    Prueba con webview con SSL

--- app/src/main/java/com/example/poc/MainActivity.java
+++ app/src/main/java/com/example/poc/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.poc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,26 +23,80 @@ public class MainActivity extends AppCompatActivity {
 
 
     private String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        url="http://www.saij.gob.ar/";
-        //url="https://192.168.0.1";
+        url="https://192.168.0.1/login.asp";
+        //url="https://www.it-swarm.dev/es/android/net-err-access-denied-android-error-al-abrir-la-pagina-web-en-android-activity/811598125/";
+
+        WebView myWebView = (WebView) findViewById(R.id.web);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        myWebView.setWebViewClient(new SSLTolerentWebViewClient());
+
+        myWebView.loadUrl(url);
+
+
+        }
+
+    private class SSLTolerentWebViewClient extends WebViewClient {
+        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            AlertDialog alertDialog = builder.create();
+            String message = "SSL Certificate error.";
+            switch (error.getPrimaryError()) {
+                case SslError.SSL_UNTRUSTED:
+                    message = "The certificate authority is not trusted.";
+                    break;
+                case SslError.SSL_EXPIRED:
+                    message = "The certificate has expired.";
+                    break;
+                case SslError.SSL_IDMISMATCH:
+                    message = "The certificate Hostname mismatch.";
+                    break;
+                case SslError.SSL_NOTYETVALID:
+                    message = "The certificate is not yet valid.";
+                    break;
+            }
+
+            message += " Do you want to continue anyway?";
+            alertDialog.setTitle("SSL Certificate Error");
+            alertDialog.setMessage(message);
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Ignore SSL certificate errors
+                    handler.proceed();
+                }
+            });
+
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    handler.cancel();
+                }
+            });
+            alertDialog.show();
+        }
     }
 
-    public void abrir(View view){
-
-        Intent intent; //declaro el intent
-        intent= new Intent(this, Main2Activity.class);
-        intent.putExtra("url", url);
-        startActivity(intent);
-    }
-
-
 
     }
+
+
+
+
+
+
 
 
 
