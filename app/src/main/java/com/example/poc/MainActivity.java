7eb1fda29571d0a57783004945b3d15771696a23
Gledys <gdys92@gmail.com>
2020-09-08 12:24:53 -0300

    Prueba con webview con SSL, pero sin mensaje de aceptacion

--- app/src/main/java/com/example/poc/MainActivity.java
+++ app/src/main/java/com/example/poc/MainActivity.java
@@ -49,43 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private class SSLTolerentWebViewClient extends WebViewClient {
         public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            AlertDialog alertDialog = builder.create();
-            String message = "SSL Certificate error.";
-            switch (error.getPrimaryError()) {
-                case SslError.SSL_UNTRUSTED:
-                    message = "The certificate authority is not trusted.";
-                    break;
-                case SslError.SSL_EXPIRED:
-                    message = "The certificate has expired.";
-                    break;
-                case SslError.SSL_IDMISMATCH:
-                    message = "The certificate Hostname mismatch.";
-                    break;
-                case SslError.SSL_NOTYETVALID:
-                    message = "The certificate is not yet valid.";
-                    break;
-            }
-
-            message += " Do you want to continue anyway?";
-            alertDialog.setTitle("SSL Certificate Error");
-            alertDialog.setMessage(message);
-            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    // Ignore SSL certificate errors
-                    handler.proceed();
-                }
-            });
-
-            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                    handler.cancel();
-                }
-            });
-            alertDialog.show();
+            handler.proceed(); // Ignore SSL certificate errors
         }
     }
 
