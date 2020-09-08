d43622e93f615321e5a9da01029016891b80ac5c
Gledys <gdys92@gmail.com>
2020-09-08 14:18:49 -0300

    Aplicacion funcionando quitando lo que no se usa

--- app/src/main/java/com/example/poc/MainActivity.java
+++ app/src/main/java/com/example/poc/MainActivity.java
@@ -1,30 +1,18 @@
 package com.example.poc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private String url;
-    //private String username;
-    //private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         url="https://192.168.0.1/login.asp";
-        //url="https://www.it-swarm.dev/es/android/net-err-access-denied-android-error-al-abrir-la-pagina-web-en-android-activity/811598125/";
-
-        //username = "admin";
-        //password = "admin";
 
 
         WebView myWebView = (WebView) findViewById(R.id.web);
@@ -45,9 +29,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setAppCacheEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setUseWideViewPort(true);
-
         myWebView.setWebViewClient(new SSLTolerentWebViewClient());
-
 
         //myWebView.loadUrl("javascript: document.getElementById('username').value = '" + username + "';" + " document.getElementById('password').value = '" + password + "';" + "var z = document.getElementById('submit').click();" );
 
